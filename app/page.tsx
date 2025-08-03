c14c539c20f511e74b25cef19e266be878f9b89e
TaehyunKim3 <agart319@gmail.com>
2025-08-03 16:31:26 +0900

    chore: 안내 문구 변경

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1>Space 458 갤러리 홈페이지 입니다.</h1>
-      <p>홈페이지 공사중입니다</p>
+      <p>홈페이지 공사중입니다 (~2025.10)</p>
       <p>그동안 새로운 소식은 아래 링크를 통해 확인해주세요</p>
       <Link
         className="underline text-lg"
